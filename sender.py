aa4ac5fe94d306d523ed4cc7091b9f3bf3c80975
nectostr <bastinda96@gmail.com>
2018-11-24 00:02:52 +0300

    "Add draft for sender module"

--- sender.py
+++ sender.py
@@ -3,10 +3,16 @@ import datetime
 
 def get_data():
     time = '2018-11-19T00:00:00'
+    old_r = []
     while(True):
         r = APIrequests.get_transprt_timestmp(time)
-        step = yield (r)
-        if step != None and step != 0:
+        tosent = []
+        for i in r:
+            if i not in old_r:
+                tosent.append(i)
+        old_r = tosent
+        step = yield (tosent)
+        if step is not None and step != 0:
             datetime_object = datetime.datetime.strptime(time[:10] + ' ' + time[11:], '%Y-%m-%d %H:%M:%S')
             time = str(datetime.timedelta(seconds=int(step)) + datetime_object)
 
@@ -14,6 +20,6 @@ def get_data():
 
 if __name__ == "__main__":
     g = get_data()
-    print('0: ' + g.send(None).text[:10])
-    print("1: " + g.send(60).text[:10])
-    print("2: " + g.send(45).text[:10])
+    print('0: ' + str(g.send(None))[:10])
+    print("1: " + str(g.send(60))[:10])
+    print("2: " + str(g.send(1))[:10])
