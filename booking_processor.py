cf874e1f8c59f542483dcd06b897e8b080c3d8a7
clevermx <klevermx@gmail.com>
2018-11-24 19:03:38 +0300

    update bookings

--- booking_processor.py
+++ booking_processor.py
@@ -149,7 +149,9 @@ def deikstra (G, start_node ,stop_node, dep_time):
                 if (dist[adj_edge[1]] > dist_delta) :
                     dist[adj_edge[1]] = dist_delta
                     if min_ind in min_paths:
-                        min_paths[adj_edge[1]] = min_paths[min_ind] + [(G.get_edge_data(adj_edge[0], adj_edge[1])[min_e]['data']["transport_number"], G.get_edge_data(adj_edge[0], adj_edge[1])[min_e]['data']["estimated_arr_datetime"])]
+                        min_paths[adj_edge[1]] = min_paths[min_ind] + [(G.get_edge_data(adj_edge[0], adj_edge[1])[min_e]['data']["transport_number"],
+                                                                        G.get_edge_data(adj_edge[0], adj_edge[1])[min_e]['data']["estimated_arr_datetime"]
+                                                                        )]
                     else:
                         min_paths[adj_edge[1]] = [(G.get_edge_data(adj_edge[0], adj_edge[1])[min_e]['data']["transport_number"], G.get_edge_data(adj_edge[0], adj_edge[1])[min_e]['data']["estimated_arr_datetime"])]
     if stop_node in min_paths:
@@ -173,7 +175,8 @@ def update_tranp_graph(G, list_of_changes):
     BOOKINGS_WAYS = {i["booking_id"]:[] for i in bookings}
 
 def update_bookings (booking_ways, G, bad_transports, cur_date):
-    dict_changes =[]
+    #get actual ways, new graph, bad_transports, and curruent date and
+    dict_changes ={}
     for ke in booking_ways:
         # ke - name of booking
         # booking_ways[ke] - list of ()
@@ -183,36 +186,38 @@ def update_bookings (booking_ways, G, bad_transports, cur_date):
         else:
             path_to_replace= []
         if path_to_replace:
-            booking= list(filter(lambda x: x["booking_id"] == ke, bookings))[0]
-            last_transport =
-            adopt_G =get_package_graph(booking, G,  ,booking["destination_station"])
+            booking_to_change= list(filter(lambda x: x["booking_id"] == ke, bookings))[0]
+            begin_with = filter(lambda x: (datetime.datetime.strptime(x[1], '%Y-%m-%dT%H:%M:%S') <  cur_date))
+            last_tr_id = begin_with[-1][0]
+            last_transport=0
+            for d in G.edges.data():
+                if d[2]["data"]["transport_number"]==last_tr_id:
+                    last_transport = d
+                    break
+            adopt_G =get_package_graph(booking_to_change, G)
+            ends_with = deikstra(adopt_G, last_transport["actual_arr_station"],booking_to_change["destination_station"] )
+            new_path = begin_with + ends_with
+            dict_changes[ke] ={ "old": booking_ways[ke], "new" : new_path, "booking" : booking_to_change}
+    return dict_changes
 
 
 
-
-
-            new_end = deikstra(adopt_G, )
-            new_path =  filter(lambda x: (datetime.datetime.strptime(x[1], '%Y-%m-%dT%H:%M:%S') < cur_date),booking_ways[ke])
-                        +
-
-            dict_changes.append( ( ke,
-                                   booking_ways[ke],
-
-
-                                  )
-                                )
 
 if __name__=="__main__":
     G = addNodes(transports)
     print ("============================")
-    G=update_tranp_graph(G, [{"transport_number": "20181112_lucky-moth-42",
-                                  "dep_station": "ORD",
-                                  "scheduled_arr_station": "JFK",
-                                  "actual_arr_station": "QWE"}])
-    print(G.edges(keys=True))
-    print("-0000000000000000")
-    print(G.edges(data=True))
+    # G=update_tranp_graph(G, [{"transport_number": "20181112_lucky-moth-42",
+    #                               "dep_station": "ORD",
+    #                               "scheduled_arr_station": "JFK",
+    #                               "actual_arr_station": "QWE"}])
+    # print(G.edges(keys=True))
+    # print("-0000000000000000")
+    # print(G.edges(data=True))
 
+    for d in G.edges.data():
+        print("11111111111111111111111111111111")
+        if d[2]["data"]["transport_number"] == "20181112_lucky-moth-42":
+            print("printing d "+str(d))
    # print(G.edges('ORD', 'JFK', '20181112_lucky-moth-42',data=True))
    #  d=deikstra(G,'ORD','MSK', datetime.datetime.strptime( '2018-01-01T14:00:00', '%Y-%m-%dT%H:%M:%S'))
    #  print(d)
