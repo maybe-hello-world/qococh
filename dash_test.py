ef2e7eac490dab7993f778922b45c20f6ec3fbb8
Kell <maybe.hello.world@gmail.com>
2018-11-24 11:56:01 +0200

    another v

--- dash_test.py
+++ dash_test.py
@@ -4,7 +4,6 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_table_experiments as dte
 import visdcc
-import pandas as pd
 
 import data_getter
 from mercator import lat_to_mercator, long_to_mercator
@@ -34,16 +33,22 @@ bookings = data_getter.g_bookings
 
 
 app.layout = html.Div([
-	visdcc.Network(
-		id='net',
-		data={'nodes': stations, 'edges': edges},
-		options=dict(
-			height='{}px'.format(MAP_HEIGHT),
-			width='{}px'.format(MAP_WIDTH),
-			physics={
-				'enabled': False
-			})
-	),
+	html.Div([
+		html.Div([
+			html.Div(id='nodes'),
+			html.Div(id='edges')
+		], style={'width': '19%', 'display': 'inline-block'}),
+		html.Div([
+		visdcc.Network(
+			id='net',
+			data={'nodes': stations, 'edges': edges},
+			options=dict(
+				height='{}px'.format(MAP_HEIGHT),
+				width='{}px'.format(MAP_WIDTH),
+				physics={
+					'enabled': False
+				})
+	)], style={'width': '80%', 'float': 'right', 'display': 'inline-block'})]),
 	dte.DataTable(
 		rows=bookings,  # initialise the rows
 		row_selectable=True,
@@ -55,6 +60,26 @@ app.layout = html.Div([
 	,
 	dcc.Interval(id='my-interval', interval=1000*1000)
 ])
+
+
+# selection for nodes
+@app.callback(
+	Output('nodes', 'children'),
+	[Input('net', 'selection')])
+def myfun(x):
+	s = 'Selected node: '
+	if len(x['nodes']) > 0: s += str(x['nodes'])
+	return s
+
+
+# selections for edges
+@app.callback(
+	Output('edges', 'children'),
+	[Input('net', 'selection')])
+def myfun(x):
+	s = 'It\'s edges: '
+	if len(x['edges']) > 0: s = [s] + [html.Div(i) for i in x['edges']]
+	return s
 
 
 @app.callback(Output('net', 'options'), [Input('my-interval', 'n_intervals')])
